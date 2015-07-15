f3e5233560262474681cedf193aa24f3c0fa02ee
Robert Cardona <rfcardona@gmail.com>
2015-07-15 16:58:43 -0400

    finished this project

--- siskel.rb
+++ siskel.rb
@@ -2,9 +2,37 @@ require 'httparty'
 
 class Siskel
   attr_reader :title
+  attr_reader :rating
+  attr_accessor :year
+  attr_reader :plot
 
-  def initialize(title)
-    @movie = HTTParty.get("http://www.omdbapi.com/?t=#{title}")
+  def initialize(title, year: "", plot: :short)
+    @movie = HTTParty.get("http://www.omdbapi.com/?t=#{title}&y=#{year}&plot=#{plot}&tomatoes=true")
     @title = @movie['Title']
+    @rating = @movie['Rated']
+    @year = @movie['Year']
+    @plot = @movie['Plot']
+    @error = @movie['Error']
   end
+
+  def title
+    if @movie['Title'].nil?
+       @error
+     else
+      @movie['Title']
+    end
+  end
+
+  def consensus
+    if @movie['tomatoMeter'].to_i <= 25
+      "Two Thumbs Down"
+    elsif @movie['tomatoMeter'].to_i <= 50
+      "Thumbs Down"
+    elsif @movie['tomatoMeter'].to_i <= 75
+      "Thumbs Up"
+    else
+      "Two Thumbs Up"
+    end
+  end
+
 end
